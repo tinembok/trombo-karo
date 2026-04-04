25e498361c23c853711a75062ad9f97da5cbf126
tinembok <tinembok@gmail.com>
2026-04-04 16:24:09 +0700

    config.js

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-
+/* TROMBO KARO - Frontend Minimal */
+var CONFIG = {
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwSKqkSjM7Ea4p0CVGXW5IC1QZj7MTwGRZ5ibFz-TzMRd_OVEdaeb3nWkB0aDPIOlFI/exec'
+};
