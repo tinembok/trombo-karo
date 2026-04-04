75bfec505334e88fc76f703d9a91d77089d5d56e
tinembok <tinembok@gmail.com>
2026-04-04 16:11:39 +0700

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-// GANTI DENGAN URL DEPLOYMENT ANDA
-var CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby0IRYx_vtYKqJoo3dO69kdx_OR34qn8V4FqOi8MKNBgb3cWPtonYMtyKAWlWtmIdz1/exec'
-};
+
